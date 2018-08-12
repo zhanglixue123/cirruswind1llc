4c10c7d4151aadbe95c662bbae5919329049250b
zhanglixue123 <38711397+zhanglixue123@users.noreply.github.com>
2018-08-11 22:05:39 -0500

    Update selectnonadjust005-700.py

--- selectnonadjust005-700.py
+++ selectnonadjust005-700.py
@@ -1,24 +1,24 @@
-r = int(input("输入日子："))
+r = int(input("Iput the date: "))
 
-print('-------------205-700-------------')
+print('-------------005-700-------------')
 for i in range((r-1)*288+2, (r-1)*288+85+1):
     print("=J%d" % (i))
 
-input('按任意键继续')
+input('Press any key to continue')
 print("\n"*100)
 
 for i in range((r-1)*288+2, (r-1)*288+85+1):
     print("=K%d" % (i))
 
 
-# # 3月份调整夏令时：
+# # summer time in March：
 # r = int(input("输入日子："))
 #
 # print('-------------205-700-------------')
 # for i in range((r-1)*288+2-12, (r-1)*288+85+1-12):
 #     print("=J%d" % (i))
 #
-# input('按任意键继续')
+# input('Press any key to continue')
 # print("\n"*100)
 #
 # for i in range((r-1)*288+2-12, (r-1)*288+85+1-12):
